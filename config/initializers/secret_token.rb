672d0a7890b2941047d5147c1a09e735c01d73f4
cflammia <flammia.carrie@gmail.com>
2015-02-09 23:35:40 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blocipedia::Application.config.secret_key_base = '0f4b084ab256b78b6b48759f34aaba8d9d5cebe6d5c2163036b2e78243bea214eb54f0e355ef850f1c9e2a9dce79c9b1388b56a0a1a8baed73bc356f780938ae'
