9aca11facd857a8f4eb2addb9f8b8d8e9e113dd3
cflammia <flammia.carrie@gmail.com>
2015-02-13 00:11:11 +0000

    Created update and edit actions and views

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -8,8 +8,32 @@ class WikisController < ApplicationController
   end
 
   def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new(params.require(:wiki).permit(:title, :body))
+    if @wiki.save
+      flash[:notice] = "Wiki saved successfully."
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error saving the wiki. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    if @wiki.update_attributes(params.require(:wiki).permit(:title, :body))
+      flash[:notice] = "Wiki updated successfully."
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error saving the wiki. Please try again."
+      render :edit
+    end
   end
 end
